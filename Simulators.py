da6a449ec6e9fab302206b4dcbf8819147082b6a
chrisbotica <botica.c@husky.neu.edu>
2019-08-06 19:31:01 -0400

    Changed pull method to gambler_model

--- Simulators.py
+++ Simulators.py
@@ -13,7 +13,7 @@ class BanditSimulator(object):
         reward_tensor = tensor([1 - reward_probability, reward_probability])
         reward = sample('arm'+str(bandit_index), dist.Categorical(reward_tensor))
         return reward
-    
+            
     def __len__(self):
         return len(self.reward_probabilities)
 
@@ -23,17 +23,21 @@ class ConfoundingBanditSimulator(object):
     def __init__(self, reward_probabilities):
         self.reward_probabilities = reward_probabilities
 
-    def pull(self, bandit_index, environment):
-        try:
-            drunk = int(environment["drunk"])
-            blinking = int(environment["blinking"])
-            reward_probability = self.reward_probabilities[bandit_index][drunk][blinking]
-            reward_tensor = tensor([1 - reward_probability, reward_probability])
-            reward = sample('arm' + str(bandit_index), dist.Categorical(reward_tensor))
-            return reward
-        except KeyError:
-            print("Needed environment for this sampler: 'drunk', 'blinking'")
+    def gambler_model(self, environment):  
+        drunk = int(environment["drunk"])
+        blinking = int(environment["blinking"])
+        intuition = int(bool(drunk) ^ bool(blinking))  # xor(drunk, blinking)
 
+        # Select arm based on intuition
+        arm = sample('arm',dist.Delta(tensor(intuition)))
+
+        # Get Reward
+        reward_probability = self.reward_probabilities[arm][drunk][blinking]
+        reward = sample('reward',dist.Bernoulli(reward_probability))
+
+        return reward
+
+    
     def __len__(self):
         return len(self.reward_probabilities)
 
